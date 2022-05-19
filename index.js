d8b3c36096c7f333b49c0d49bc7dce3055600c54
Victor <vx_zz@hotmail.com>
2022-05-19 05:59:08 -0400

    commit tree 41d872a256dbcec5539fb8751690e3159fe01ed5

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const array = []
+    for (let i = 0; i < names.length; i++){
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return array
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
